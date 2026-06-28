c1657a4800f8be728ce6d9302c59f6aea99c4f22
Nana <mariananavarro3022@gmail.com>
2026-06-28 00:41:59 +0000

    modificado

---
+++ generar.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+INJAX="/home/nana/Documents/Visualizacion de Informacion/Proyecto2Visualizacion/injax"
+"$INJAX" plan_materiales_1217.json red_cursos.inja red_cursos.svg && echo "SVG actualizado"
